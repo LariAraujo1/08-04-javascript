e48027189762c45c57021309caadc7c46bbc46e7
LARISSA TAMIRES DE ARAUJO NASCIMENTO <larissa.tanascimento@senacsp.edu.br>
2024-04-08 16:17:24 -0300

    Tabuada-dinamica

--- js/script.js
+++ js/script.js
@@ -1,50 +1,43 @@
-const multiplicationForm = document.querySelector("#multiplication-form")
+// Seleção de elementos
 
-const numberInput = document.querySelector("#number")
-
-const multiplicationInput = document.querySelector("#multiplicator")
-
-const multiplicationTitle = document.querySelector("#multiplication-title span")
-
-const multiplicationTable = document.querySelector("#multiplication-operations")
-
+// 01 - Selecionando o formulário
+const multiplicationForm = document.querySelector("#multiplication-form");
+// 02 - Numero da tabuada
+const numberInput = document.querySelector("#number");
+// 03 - Quanta vezes desejamos multiplicar
+const multiplicationInput = document.querySelector("#multiplicator");
 
 
+// 05 - Limpando os campos "#multiplication-title span" e #multiplication-operations"
+const multiplicationTitle = document.querySelector(
+  "#multiplication-title span"
+);
+const multiplicationTable = document.querySelector(
+  "#multiplication-operations"
+);
 
-//evento
+// Funções
 
-multiplicationForm.addEventListener("submit", (e)=>{ 
-    //para não recarregar a pagina quando clica no botão calcular- ele retira o reload da pagina:
-    e.preventDefault();
+// 06 - Criando a função que  cria tabela
+const createTable = (number, multiplicatorNumber) => {
+  multiplicationTable.innerHTML = ""; // Acesso o conteudo de texto e subtituo por string vazia
 
-    const multiplicationNumber = numberInput.value;//numero inserido da tabuada
+  //criando a tabuada
+  for (i = 1; i <= multiplicatorNumber; i++) {
+    const result = number * i;
 
-    const multiplicatorNumber = +multiplicationInput.value;//Quantas vezes eu vou calcular o numero inserido
-
-    if(!multiplicationNumber || !multiplicatorNumber) return; // só sera executado quando ambos os campos estiverem preenchidos
-
-createTable(multiplicationNumber, multiplicatorNumber);//vamor criar a função que cria a tabela mas para isso,precisamos garantir que os campos #multiplication-title span e #multiplication-operations estejam vazios...
-})
-
-const createTable= (number, multiplicatorNumber)=>{
-    multiplicationTable.innerHTML ="";
-    //Acesso o conteudo de texto e substituo por string vazia 
-
-    //criando a tabuada
-    for(i=1; i<=multiplicatorNumber; i++){
-        const result= number * i;
-
-        const template = <div clss="row">
-            <div class="operation">${number} x ${i}=</div>
+    const template = `<div class="row">
+            <div class="operation">${number} x ${i} = </div>
             <div class="result">${result}</div>
-        </div>
-    const parser= new DOMParser();//Objeto que perimite transferir um string em html
-    const htmlTemplate= parser.parseFromString(template, "text/html");
-    const row= htmlTemplate.querySelector(".row");
-    multiplicationTable.appendChild(row);
+        </div>`;
 
-    }
-    multiplicationTitle.innerText = number;
+    const parser = new DOMParser(); // Obejto que permite transferir um string em html
+    const htmlTemplate = parser.parseFromString(template, "text/html");
+    const row = htmlTemplate.querySelector(".row");
+    multiplicationTable.appendChild(row);
+  }
+
+  multiplicationTitle.innerText = number;
 };
 
 
@@ -53,19 +46,13 @@ const createTable= (number, multiplicatorNumber)=>{
 
 // 04
 multiplicationForm.addEventListener("submit", (e) => {
-    e.preventDefault(); //Quando clicar no botção calcular, não quero que ele recarregue a página
-  
-    const multiplicationNumber = numberInput.value; // numero inserido da tabuada
-  
-    const multiplicatorNumber = +multiplicationInput.value; // Quantas vezes eu vou calcular o número inserido
-  
-    if (!multiplicationNumber || !multiplicatorNumber) return; // Só será executado se ambos os campos estiverem preenchidos
-  
-    createTable(multiplicationNumber, multiplicatorNumber); // vamos criar a função que cria a tabela mas para isso, precisamos garantir  que o campos "#multiplication-title span" e "#multiplication-operations" estejam vazios..... vá para o 05
-  });
+  e.preventDefault(); //Quando clicar no botção calcular, não quero que ele recarregue a página
 
+  const multiplicationNumber = numberInput.value; // numero inserido da tabuada
 
+  const multiplicatorNumber = +multiplicationInput.value; // Quantas vezes eu vou calcular o número inserido
 
+  if (!multiplicationNumber || !multiplicatorNumber) return; // Só será executado se ambos os campos estiverem preenchidos
 
-
-
+  createTable(multiplicationNumber, multiplicatorNumber); // vamos criar a função que cria a tabela mas para isso, precisamos garantir  que o campos "#multiplication-title span" e "#multiplication-operations" estejam vazios..... vá para o 05
+});
---
+++ readme.md
@@ -0,0 +1,26 @@
+# Tabuada Dinãmica
+
+Um aplicativo simple que gera uma tabela de tabuada de multiplicação dinâmica com base nos números fornecidos pelo usuário.
+
+## Funcionalidades
+
+-O usuario pode inserir um numero da tabuada desejado
+-o usuario pode especificar quantas vezes o numero da tabuada deve ser multiplicado.
+-uma tabela de tabuada é gerada dinamicamente com base nas entradas do usuario.
+-a tabela é exibida na pagina em tempo real.
+
+## Como usar
+1. clone este repositorio para seu computador: https://github.com/LariAraujo1/08-04-javascript.git
+2. Navegue até o diretporio do projeto: cd tabuada dinamica
+3. Abra o arquivo 'index.html' em um navegador da web.
+4. insira o numero da tabuada desejado e a quantidade de vezes  que o numero deve ser multiplicado nos campos apropriados.
+5.clique no botão "calcular" para gerar a tabela de tabuada.
+6. A tabela de tabuada será exibida abaixo do formulario.
+
+## Tecnologias utilizadas
+- html
+- css
+-javaScript
+
+#Contribuição
+
